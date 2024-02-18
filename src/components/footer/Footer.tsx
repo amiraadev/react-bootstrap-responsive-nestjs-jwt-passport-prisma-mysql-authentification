a6dd55bb09fd573c0d388458c99fe30fc26c6482
yakinmohammed <allagui_fawzi@outlook.fr>
2024-02-18 19:24:22 +0100

    finishing the Footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,75 +5,78 @@ import "./footer.css";
 import Button from "react-bootstrap/Button";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+import { Link } from "react-router-dom";
+
+import { FaSquareFacebook } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa6";
 
 const Footer = () => {
 	return (
 		<>
 			<Navbar bg='light ' variant={"light"} expand='lg'>
-				<Container className='justify-content-between'>
-					<div className='col-md-6 col-lg-5 col-12 ft-1'>
-						<h3>
-							<span>round</span>desk
-						</h3>
-						<p>
-							Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum
-							ea quo ex ullam laboriosam magni totam, facere eos iure voluptate.
-						</p>
-						<div className='footer-icons'>
-							<i className='fa-brands fa-facebook'></i>
-							<i className='fa-brands fa-twitter'></i>
-							<i className='fa-brands fa-instagram'></i>
-							<i className='fa-brands fa-linkedin-in'></i>
-						</div>
-					</div>
-					<div className='col-md-6 col-lg-3 col-12 ft-2'>
-						<h5>Quick Links</h5>
-						<ul>
-							<li className='nav-item'>
-								<a className='' href='/'>
-									Services
-								</a>
-							</li>
-							<li className='nav-item'>
-								<a className='' href='/'>
-									Portfolio
-								</a>
-							</li>
-							<li className='nav-item'>
-								<a className='' href='/'>
-									Contact Us
-								</a>
-							</li>
-							<li className='nav-item'>
-								<a className='' href='/'>
-									Services
-								</a>
-							</li>
-							<li className='nav-item'>
-								<a className='' href='/'>
-									Portfolio
-								</a>
-							</li>
-						</ul>
-					</div>
-					<div className='col-md-6 col-lg-4 col-12 ft-3'>
-						<h5>Quick Links</h5>
-						<p>
-							<i className='fa-solid fa-phone-volume'></i> +92 3121324083
-						</p>
-						<p>
-							<i className='fa-solid fa-envelope'></i> allaguiamira@gmail.com
-						</p>
-						<p>
-							<i className='fa-solid fa-paper-plane'></i> Allagui, Tunisia.
-						</p>
-					</div>
+				<Container className='d-flex align-items-center justify-content-center'>
+					<Row>
+						<Col
+							className='d-flex flex-column align-items-center justify-content-center ft-1 m-4'
+							xs={12}
+							md={3}>
+							<h3>
+								<span className='title-span'>round</span>desk
+							</h3>
+							<div className=' px-3'>
+								<span>
+									Roundesk Technologies is a new start-up that targets SMEs,
+									ETIs and Large Enterprises through an omnichannel and
+									cross-channel Customer Relationship Management tool.
+								</span>
+							</div>
+						</Col>
+						<Col
+							className='d-flex flex-column align-items-center justify-content-center ft-1 m-4'
+							xs={12}
+							md={3}>
+							<div className='footer-icons'>
+								<Link to='/about'>
+									<FaSquareFacebook className='react-icon-class' />
+								</Link>
+								<Link to='/about'>
+									<FaGithub className='react-icon-class' />
+								</Link>
+								<Link to='/about'>
+									<FaLinkedin className='react-icon-class' />
+								</Link>
+							</div>
+						</Col>
+
+						<Col
+							className='d-flex align-items-center justify-content-center  m-4'
+							xs={12}
+							md={3}>
+							<div className='d-flex flex-column align-items-center justify-content-center ft-1'>
+								<h5>Contact</h5>
+								<ul>
+									<li className='nav-item'>
+										<i className='fa-solid fa-phone-volume'></i> +92 3121324083
+									</li>
+									<li className='nav-item'>
+										<i className='fa-solid fa-envelope'></i>{" "}
+										allaguiamira@gmail.com
+									</li>
+									<li className='nav-item'>
+										<p>
+											<i className='fa-solid fa-paper-plane'></i> Allagui,
+											Tunisia.
+										</p>
+									</li>
+								</ul>
+							</div>
+						</Col>
+					</Row>
 				</Container>
-				<div className='Footer'>
-					<div className='container'>
-						<div className='row'></div>
-					</div>
-				</div>
 			</Navbar>
 			<div className='Last-footer'>
 				<p>Designed By Amira</p>
