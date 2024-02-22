9c7ef4fccebbf5dc874ab62c4fb782050113a1cd
amiraadev <allagui_fawzi@outlook.fr>
2024-02-22 23:26:19 +0100

    navbar toggle

--- src/components/Navbar1.tsx
+++ src/components/Navbar1.tsx
@@ -1,61 +1,55 @@
 /** @format */
 
-import Container from "react-bootstrap/Container";
+// /** @format */
+
 import Button from "./Button";
 import Navbar from "react-bootstrap/Navbar";
 import Form from "react-bootstrap/Form";
-import InputGroup from "react-bootstrap/InputGroup";
-// import Button from "react-bootstrap/Button";
 import DarkMode from "./DarkMode/DarkMode";
 
 import useThemeStore from "../stores/themeStore";
-import { Col, Row } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
 const Navbar1 = () => {
+	const navigate = useNavigate();
 	const { isDarkMode } = useThemeStore();
 	return (
-		<Navbar className='bg-body-tertiary justify-content-between'>
+		<Navbar
+			bg={`${isDarkMode ? "dark" : "light"}`}
+			variant={`${isDarkMode ? "dark" : "light"}`}
+			className={`justify-content-between `}
+			collapseOnSelect
+			expand='lg'>
 			<Form>
-				<img src='./roundesk-logo.png' className='logo-img' />
+				<img
+					src='./roundesk-logo.png'
+					alt='alt'
+					className='logo-img'
+					onClick={() => {
+						navigate("/");
+					}}
+				/>
 			</Form>
 			<Form>
-				<Row>
-					<Col className="p-1">
-						<DarkMode />
-					</Col>
-					<Col  className='d-flex flex'>
-						<Button type='submit' label='submit' onClick={() => {}} />
+				<Navbar.Toggle aria-controls='responsive-navbar-nav' />
+				<Navbar.Collapse id='responsive-navbar-nav'>
+					<div className='d-flex justify-content-center'>
+						<div className='d-flex align-items-center dark-mode-class'>
+							<DarkMode />
+						</div>
 						<Button
 							outline={true}
 							type='submit'
 							label='submit'
 							onClick={() => {}}
-						/>
-					</Col>
-				</Row>
+						/>{" "}
+						<Button type='submit' label='submit' onClick={() => {}} />
+					</div>
+				</Navbar.Collapse>
 			</Form>
 		</Navbar>
-		// <Navbar
-		// 	bg={`${isDarkMode ? "dark" : "light"}`}
-		// 	variant={`${isDarkMode ? "dark" : "light"}`}
-		// 	className="justify-content-between"
-		// 	>
-		// 	<Container fluid style={{backgroundColor:"red"}}>
-		// 		<Row fluid style={{backgroundColor:"yellow"}}>
-		// 			<Col >her</Col>
-		// 			<Col >vvv</Col>
-		// 		</Row>
-		// 	</Container>
-		// 	<Container className='justify-content-between'>
-		// 		<div >
-		// 			<img src='./roundesk-logo.png' className='logo-img' />
-		// 		</div>
-		// 		<div >
-		// 		<DarkMode />
-
-		// 		</div>
-
-		// 	</Container>
-		// </Navbar>
 	);
 };
 
