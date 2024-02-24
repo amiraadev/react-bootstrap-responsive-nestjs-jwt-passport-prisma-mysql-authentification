3e93a56bf453871a467a4f7d292535fc3d80f239
amiraadev <allagui_fawzi@outlook.fr>
2024-02-24 18:00:28 +0100

    docker-compose : mysql service

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,22 +1,24 @@
-import { Controller ,Post,Body, HttpException, UseGuards,Get} from '@nestjs/common';
+import {  Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuthPayloadDto } from './dto/auth.dto';
 import { LocalGuard } from './guards/local.guard';
+import { Request } from 'express';
+import { JwtAuthGuard } from './guards/jwt.guard';
 
 @Controller('/auth')
 export class AuthController {
     constructor(private readonly usersService: AuthService) {}
 
-    @Post('/login')
-    @UseGuards(LocalGuard)
-    login(@Body() authPayload:AuthPayloadDto){
-     const user = this.usersService.validateUser(authPayload);
-     if(!user) throw new HttpException('Invalid Credentials', 401);
-     return user;
-    }
+  @Post('login')
+  @UseGuards(LocalGuard)
+  login(@Req() req: Request) {
+    return req.user;
+  }
 
     @Get('status')
-    status(@Req() req: Request){
-        req.user
+    @UseGuards(JwtAuthGuard)
+    status(@Req() req: Request) {
+      console.log('Inside AuthController status method');
+      console.log(req.user);
+      return req.user;
     }
 }
