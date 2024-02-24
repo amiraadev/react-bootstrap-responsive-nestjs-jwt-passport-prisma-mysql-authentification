e8f754712e7f18e32e9d97b1692285c7484b776f
amiraadev <allagui_fawzi@outlook.fr>
2024-02-24 02:10:44 +0100

    sign the jwt

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller ,Post,Body} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthPayloadDto } from './dto/auth.dto';
 
-@Controller('auth')
-export class AuthController {}
+@Controller('/auth')
+export class AuthController {
+    constructor(private readonly usersService: AuthService) {}
+
+    @Post('/login')
+    login(@Body() authPayload:AuthPayloadDto){
+     return this.usersService.validateUser(authPayload);
+    }
+}
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { AuthPayloadDto } from './dto/auth.dto';
+import { JwtService } from '@nestjs/jwt';
 
+const fakeUsers = [
+  {
+    id: 1,
+    username: 'anson',
+    password: 'password',
+  },
+  {
+    id: 2,
+    username: 'anson',
+    password: 'password',
+  },
+];
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+constructor(private jwtService:JwtService ){}
+
+  validateUser({username,password}: AuthPayloadDto) {
+     const findUser = fakeUsers.find((user) => user.username === username);
+     if(!findUser){
+        return null;
+     }
+     if(password === findUser.password){
+        const {password,...user} = findUser;
+       return this.jwtService.sign(user)
+     }
+  }
+}
